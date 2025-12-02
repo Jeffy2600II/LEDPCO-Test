87280aa65a9d7f6b2a32f2b2a6e0aa35aafca0db
Jeffy2600II <nontakorn2600@gmail.com>
2025-12-02 11:09:58 +0700

    update

--- api/webhook.ts
+++
@@ -1,7 +0,0 @@
-// สำหรับ Vercel Serverless Function
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { app } from '../src/index';
-
-export default async (req: VercelRequest, res: VercelResponse) => {
-  return app(req, res);
-};
